00cb75d27c675c8cec7736285f2118f01aec9c34
/azeem <136465208+Aze0029@users.noreply.github.com>
2023-06-16 10:23:40 -0400

    Poison.sh

--- Poison.sh
+++ Poison.sh
@@ -1,1 +1,12 @@
-echo -e "\e[1;35mRED MESSAGE \e[0m"
+clear
+echo -e "\e[1;35m             _                 
+            (_)                
+ _ __   ___  _ ___  ___  _ __  
+| '_ \ / _ \| / __|/ _ \| '_ \ 
+| |_) | (_) | \__ \ (_) | | | |
+| .__/ \___/|_|___/\___/|_| |_|
+| |                            
+|_|                            
+echo "Type bash run.sh to run poison"
+echo "DISCLAIMER: The creator of poison is not reponsible for any damage caused by it to ChromeOS"
+\e[0m"
